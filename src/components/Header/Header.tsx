846f70de1aaf215f44728ba202f5db2799df2088
Psinka <Psiniy-programmer@yandex.ru>
2021-06-12 16:08:42 +0300

    шапка

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {CustomSelectInterface} from "@components/CustomSelect";
+import "./Header.scss";
+
+const selectWidth: number = 167;
+
+const selects: CustomSelectInterface[] = [
+  {
+    width: selectWidth,
+    id: {
+
+    },
+    label: {
+      '2'
+    },
+  }
+];
+
+const Header = () => {
+
+  return (
+    <header>
+      
+    </header>
+  );
+};
+
+export default Header;
