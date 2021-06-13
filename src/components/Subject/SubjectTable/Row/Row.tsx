47b9d8b287947d6ddb7636228975179d63dedc9a
psiniy-programmer <psiniy-programmer@yandex.ru>
2021-06-13 03:17:04 +0300

    autocomplete

--- src/components/Subject/SubjectTable/Row/Row.tsx
+++ src/components/Subject/SubjectTable/Row/Row.tsx
@@ -1,16 +1,72 @@
+import { useState } from "react";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
 import styles from "./Row.module.scss";
-    // grid-template-columns: 42px 163px 450px 400px 213px 132px;
 
-const Row = () => {
+interface RowProps {
+  number: number;
+  timer: string;
+}
+
+type ISubj = {
+  name: string
+}
+
+// ! Сюда подгрузим список предметов 
+const options: ISubj[] = [
+  {
+    name: "keker",
+  },
+];
+
+const Row = ({ number, timer }: RowProps) => {
+  const [double, setDouble] = useState(false);
+
+  const doubleHandler = () => {
+    setDouble(!double);
+    console.log(double);
+  };
+
   return (
     <tr className={styles.tr}>
-      <td width={42} className={styles.td}>1</td>
-      <td width={121} className={styles.td}>2</td>
-      <td width={42} className={styles.td}>3</td>
-      <td width={442} className={styles.td}>4</td>
-      <td width={392} className={styles.td}>5</td>
-      <td width={213} className={styles.td}>6</td>
-      <td width={124} className={styles.td}>7</td>
+      <td width={42} className={styles.td}>
+        {number}
+      </td>
+      <td width={121} className={styles.td}>
+        {timer}
+      </td>
+      <td width={42} className={styles.td}>
+        <Checkbox color="primary" checked={double} onChange={doubleHandler} />
+      </td>
+      <td width={442} className={styles.td}>
+        <Autocomplete
+          id="combo-box-demo"
+          options={options}
+          getOptionLabel={(option) => option.name}
+          style={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="" variant="outlined" />
+          )}
+        />
+      </td>
+      <td width={392} className={styles.td}>
+      <Autocomplete
+          id="combo-box-demo"
+          options={options}
+          getOptionLabel={(option) => option.name}
+          style={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField {...params} label="" variant="outlined" />
+          )}
+        />
+      </td>
+      <td width={213} className={styles.td}>
+        6
+      </td>
+      <td width={124} className={styles.td}>
+        7
+      </td>
     </tr>
   );
 };
--- src/components/Subject/SubjectTable/SubjectTable.tsx
+++ src/components/Subject/SubjectTable/SubjectTable.tsx
@@ -2,8 +2,11 @@ import styles from "./SubjectTable.module.scss";
 import Naming from "./Naming";
 import Row from "./Row";
 import Header from "./Header";
+import { useTypedSelector } from "../../../hooks/useTypedSelector";
 
 const SubjectTable = () => {
+  const timings = useTypedSelector((state) => state.timing);
+
   return (
     <table className={styles.table}>
       <thead>
@@ -11,7 +14,9 @@ const SubjectTable = () => {
         <Naming />
       </thead>
       <tbody>
-        <Row />
+        {timings.map((timer, index) => (
+          <Row key={timer} number={index + 1} timer={timer} />
+        ))}
       </tbody>
     </table>
   );
