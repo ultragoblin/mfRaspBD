fc02499de03d2978333b316d54cd1ea2d373e5e9
psina <psiniy-programmer@yandex.ru>
2021-07-30 13:44:15 +0300

    stateFunc added

--- src/components/Subject/SubjectTable/Row/Double/Double.tsx
+++ src/components/Subject/SubjectTable/Row/Double/Double.tsx
@@ -1,87 +1,94 @@
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import Checkbox from "@material-ui/core/Checkbox";
 import TextField from "@material-ui/core/TextField";
-import { RowChildProps, RowWidth } from "../Row";
+import {RowChildProps, RowWidth} from "../Row";
 import Subgroup from "../Subgroup";
 import Aud from "../Aud";
 
-const Double = ({ state, timer, handler, number, options: {subGroup, aud, teacher, subject} }: RowChildProps) => {
-  const { AUD, SUBJECT, TIMER, SUBGROUP, CHECKBOX, NUMBER, TEACHER } = RowWidth;
+const Double = ({
+                    state,
+                    timer,
+                    handler,
+                    stateFunc,
+                    number,
+                    options: {subGroup, aud, teacher, subject}
+                }: RowChildProps) => {
+    const {AUD, SUBJECT, TIMER, SUBGROUP, CHECKBOX, NUMBER, TEACHER} = RowWidth;
 
-  const handleChange = () => {
+    const handleChange = () => {
 
-  }
+    }
 
-  return (
-    <>
-      <tr className="tr">
-        <td rowSpan={2} width={NUMBER} className="tr">
-          {number}
-        </td>
-        <td rowSpan={2} width={TIMER} className="td">
-          {timer}
-        </td>
-        <td rowSpan={2} width={CHECKBOX} className="td">
-          <Checkbox color="primary" checked={state} onChange={handler} />
-        </td>
-        <td width={SUBJECT} className="td">
-          <Autocomplete
-            options={subject}
-            style={{ width: SUBJECT }}
-            getOptionLabel={(option) => option.subject}
-            renderInput={(params) => (
-              <TextField {...params} label="" variant="outlined" />
-            )}
-          />
-        </td>
-        <td width={TEACHER} className="td">
-          <Autocomplete
-            options={teacher}
-            getOptionLabel={(option) => option.teacher}
-            style={{ width: TEACHER }}
-            renderInput={(params) => (
-              <TextField {...params} label="" variant="outlined" />
-            )}
-          />
-        </td>
-        <td width={AUD} className="td">
-          <Aud handleFunc={handleChange} options={aud} />
-        </td>
-        <td width={SUBGROUP} className="td">
-          <Subgroup options={subGroup} />
-        </td>
-      </tr>
+    return (
+        <>
+            <tr className="tr">
+                <td rowSpan={2} width={NUMBER} className="tr">
+                    {number}
+                </td>
+                <td rowSpan={2} width={TIMER} className="td">
+                    {timer}
+                </td>
+                <td rowSpan={2} width={CHECKBOX} className="td">
+                    <Checkbox color="primary" checked={state} onChange={handler}/>
+                </td>
+                <td width={SUBJECT} className="td">
+                    <Autocomplete
+                        options={subject}
+                        style={{width: SUBJECT}}
+                        getOptionLabel={(option) => option.subject}
+                        renderInput={(params) => (
+                            <TextField {...params} label="" variant="outlined"/>
+                        )}
+                    />
+                </td>
+                <td width={TEACHER} className="td">
+                    <Autocomplete
+                        options={teacher}
+                        getOptionLabel={(option) => option.teacher}
+                        style={{width: TEACHER}}
+                        renderInput={(params) => (
+                            <TextField {...params} label="" variant="outlined"/>
+                        )}
+                    />
+                </td>
+                <td width={AUD} className="td">
+                    <Aud handleFunc={handleChange} options={aud}/>
+                </td>
+                <td width={SUBGROUP} className="td">
+                    <Subgroup options={subGroup}/>
+                </td>
+            </tr>
 
-      <tr>
-        <td width={SUBJECT} className="td__dobule">
-          <Autocomplete
-            options={subject}
-            style={{ width: SUBJECT }}
-            getOptionLabel={(option) => option.subject}
-            renderInput={(params) => (
-              <TextField {...params} label="" variant="outlined" />
-            )}
-          />
-        </td>
-        <td width={TEACHER} className="td__dobule">
-          <Autocomplete
-            options={teacher}
-            getOptionLabel={(option) => option.teacher}
-            style={{ width: TEACHER }}
-            renderInput={(params) => (
-              <TextField {...params} label="" variant="outlined" />
-            )}
-          />
-        </td>
-        <td width={AUD} className="td__dobule">
-          <Aud handleFunc={handleChange} options={aud} />
-        </td>
-        <td width={SUBGROUP} className="td__dobule">
-          <Subgroup options={subGroup} />
-        </td>
-      </tr>
-    </>
-  );
+            <tr>
+                <td width={SUBJECT} className="td__dobule">
+                    <Autocomplete
+                        options={subject}
+                        style={{width: SUBJECT}}
+                        getOptionLabel={(option) => option.subject}
+                        renderInput={(params) => (
+                            <TextField {...params} label="" variant="outlined"/>
+                        )}
+                    />
+                </td>
+                <td width={TEACHER} className="td__dobule">
+                    <Autocomplete
+                        options={teacher}
+                        getOptionLabel={(option) => option.teacher}
+                        style={{width: TEACHER}}
+                        renderInput={(params) => (
+                            <TextField {...params} label="" variant="outlined"/>
+                        )}
+                    />
+                </td>
+                <td width={AUD} className="td__dobule">
+                    <Aud handleFunc={handleChange} options={aud}/>
+                </td>
+                <td width={SUBGROUP} className="td__dobule">
+                    <Subgroup options={subGroup}/>
+                </td>
+            </tr>
+        </>
+    );
 };
 
 export default Double;
