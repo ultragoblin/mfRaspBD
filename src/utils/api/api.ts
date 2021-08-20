753c440c7fae06627d6a92ac536a65624e9aa871
ultragoblin <ultragoblin@users.noreply.github.com>
2021-08-20 08:45:39 +0300

    Update api.ts
    links to proxy

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -3,31 +3,31 @@
 import {TGroupInfo} from "../../Redux/reducers/raspData";
 
 const api = {
-  fullList: 'https://rasp.msfu.ru/api/adm/full_list',
-  pairList: 'https://rasp.msfu.ru/api/adm/pair/list',
-  settings: 'https://rasp.msfu.ru/api/adm/settings',
+  fullList: '/api/adm/full_list',
+  pairList: '/api/adm/pair/list',
+  settings: '/api/adm/settings',
   admLists: {
-    teacher: 'https://rasp.msfu.ru/api/adm/teacher/list',
-    aud: 'https://rasp.msfu.ru/api/adm/aud/list',
-    subject: 'https://rasp.msfu.ru/api/adm/subject/list',
-    group: 'https://rasp.msfu.ru/api/adm/group/list',
-    subgroup: 'https://rasp.msfu.ru/api/adm/subgroup/list',
-    caf: 'https://rasp.msfu.ru/api/adm/caf/list',
-    stage: 'https://rasp.msfu.ru/api/adm/stage/list',
-    faculty: 'https://rasp.msfu.ru/api/adm/faculty/list',
-    year: 'https://rasp.msfu.ru/api/adm/year/list'
+    teacher: '/api/adm/teacher/list',
+    aud: '/api/adm/aud/list',
+    subject: '/api/adm/subject/list',
+    group: '/api/adm/group/list',
+    subgroup: '/api/adm/subgroup/list',
+    caf: '/api/adm/caf/list',
+    stage: '/api/adm/stage/list',
+    faculty: '/api/adm/faculty/list',
+    year: '/api/adm/year/list'
   },
   DB: {
-    group: 'https://rasp.msfu.ru/api/adm/group',
-    subject: 'https://rasp.msfu.ru/api/adm/subject',
-    teacher: 'https://rasp.msfu.ru/api/adm/teacher',
-    aud: 'https://rasp.msfu.ru/api/adm/aud'
+    group: '/api/adm/group',
+    subject: '/api/adm/subject',
+    teacher: '/api/adm/teacher',
+    aud: '/api/adm/aud'
   },
   getGroup: ({
                group,
                year,
                semester
-             }: TGroupInfo) => `https://rasp.msfu.ru/api/adm/group?id=${group}&year=${year}&semester=${semester}`
+             }: TGroupInfo) => `/api/adm/group?id=${group}&year=${year}&semester=${semester}`
 };
 
 export const authDefault: RequestInit | undefined = {
